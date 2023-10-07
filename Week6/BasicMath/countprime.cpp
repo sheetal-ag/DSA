f5b3366d9c556d928b2ec661499b5387c99d0e56
Sheetal Agrahari <145831862+sheetal-ag@users.noreply.github.com>
2023-10-08 01:17:22 +0545

    Update countprime.cpp

--- Week6/BasicMath/countprime.cpp
+++ Week6/BasicMath/countprime.cpp
@@ -1,7 +1,7 @@
 /*204. Count Primes
 
 Given an integer n, return the number of prime numbers that are strictly less than n*/
-//Naive Approach O(n^2)
+//NAIVE APPROACH O(n^2)
 
 #include<iostream>
 using namespace std;
@@ -13,6 +13,51 @@ class Solution
         if(n<=1)
          return false;
         for(int i=2;i<n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+int countPrimes(int n){
+    int c=0;
+    int count;
+
+    for(int i=0;i<n;i++){
+        if (isPrime(i)){
+            c++;
+        }
+    }
+    return c;
+    
+}
+
+};
+
+int main(){
+    Solution myObj;
+   cout<< myObj.countPrimes(n);
+   return  0;
+
+
+}
+
+
+//2)SQRT APPROACH O(nsqrtn)
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class Solution
+{
+    public:
+    bool isPrime(int n){
+        if(n<=1)
+         return false;
+        int sqrtN=sqrt(n);
+        for(int i=2;i<=sqrtN;i++){
             if(n%i==0){
                 return false;
             }

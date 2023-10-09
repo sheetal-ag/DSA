8210e5e1abb08891351fcef75f7bfc74d227c3a6
Sheetal Agrahari <145831862+sheetal-ag@users.noreply.github.com>
2023-10-09 17:23:24 +0545

    Update exponentiation.cpp

--- Week6/BasicMath/exponentiation.cpp
+++ Week6/BasicMath/exponentiation.cpp
@@ -1,4 +1,4 @@
-//Slow Exponentiation(Naive Approach) O(b)
+//1)Slow Exponentiation(Naive Approach) O(b)
 #include<iostream>
 using namespace std;
 
@@ -16,3 +16,23 @@ cout<<slowExponentiation(5,4)<<endl;
 return 0;
 }
   
+//2)Fast Exponentiation O(log b)
+#include<iostream>
+using namespace std;
+
+int fastExponentiation(int a,int b){
+int ans=1;
+  while(b>0)
+  { if(b&1){
+    ans=ans*a;  //odd
+   }
+   a=a*a;
+   b>>=1;
+  }
+  return ans;
+}
+
+int main(){
+cout<<fastExponentiation(5,4)<<endl;
+return 0;
+}

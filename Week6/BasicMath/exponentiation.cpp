e8347b70f0723083c3678f8055587818f6d3179e
Sheetal Agrahari <145831862+sheetal-ag@users.noreply.github.com>
2023-10-09 15:57:44 +0545

    Update exponentiation.cpp

--- Week6/BasicMath/exponentiation.cpp
+++ Week6/BasicMath/exponentiation.cpp
@@ -1,1 +1,18 @@
+//Slow Exponentiation(Naive Approach)
+#include<iostream>
+using namespace std;
 
+int slowExponentiation(int a,int b){
+int ans=1;
+  for(int i=0;i<b;i++)
+  {
+      ans*=a;
+   }
+  return ans;
+}
+
+int main(){
+cout<<slowExponentiation(5,4)<<endl;
+return 0;
+}
+  

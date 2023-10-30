8f435f8fbb6e7d29c9a1c524fc023e7df2ad46f0
Sheetal Agrahari <145831862+sheetal-ag@users.noreply.github.com>
2023-10-30 16:11:10 +0545

    Update localglobal.cpp

--- Week 9/localglobal.cpp
+++ Week 9/localglobal.cpp
@@ -1,1 +1,12 @@
+#include<stdio.h>
+using namespace std;
 
+int x=2;//global
+
+int main(){
+  x=2;
+int x=20;        //local
+cout<<x<<endl;   //local variable
+cout<<::x<<endl;  //accessing global variable value
+return 0;
+}

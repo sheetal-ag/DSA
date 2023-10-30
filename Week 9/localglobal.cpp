003b8fa855d85b52bce0f2666f33f8d9f02116b8
Sheetal Agrahari <145831862+sheetal-ag@users.noreply.github.com>
2023-10-30 16:18:23 +0545

    Update localglobal.cpp

--- Week 9/localglobal.cpp
+++ Week 9/localglobal.cpp
@@ -8,5 +8,13 @@ int main(){
 int x=20;        //local
 cout<<x<<endl;   //local variable
 cout<<::x<<endl;  //accessing global variable value
+{ int x=50;
+   {
+     int x=44;
+     cout<<x<<endl;  //scoped so prints 44
+    }
+ cout<<x<<endl; //outer scoped so prints 50
+ cout<<::x<<endl;//prints 2
+}
 return 0;
 }

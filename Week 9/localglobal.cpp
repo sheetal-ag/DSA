bb45f01d2b2f803f511d7018bc82ac4816ed6e75
Sheetal Agrahari <145831862+sheetal-ag@users.noreply.github.com>
2023-10-30 16:13:29 +0545

    Update localglobal.cpp

--- Week 9/localglobal.cpp
+++ Week 9/localglobal.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int x=2;//global
 
 int main(){
-  x=2;
+  x=2; //or ::x=2;
 int x=20;        //local
 cout<<x<<endl;   //local variable
 cout<<::x<<endl;  //accessing global variable value

fe83ec803e36916755308a1af354b053a7b78899
Sheetal Agrahari <145831862+sheetal-ag@users.noreply.github.com>
2023-11-06 22:32:20 +0545

    Update arr1.cpp

--- Week 3/arr1.cpp
+++ Week 3/arr1.cpp
@@ -8,7 +8,7 @@ void printArray(int arr[], int size) {
   }
   cout << endl;
 }
-
+/* ******Linear search***** *\
 //present -> true;
 //absent -> false;
 
@@ -39,3 +39,41 @@ cout<<"Target not found"<<endl;
 
 return 0;
 }
+
+
+//COUNT 0s AND 1s IN THE ARRAY
+void countZeroOne(int arr[], int size) {
+  int zeroCount = 0;
+  int oneCount = 0;
+
+  for(int i=0; i<size; i++) {
+    if(arr[i] == 0) {
+      zeroCount++;
+    }
+    if(arr[i] == 1) {
+      oneCount++;
+    }
+  }
+
+  cout << "ZeroCount: " << zeroCount << endl;
+  cout << "oneCOunt: " << oneCount << endl;
+  
+}
+
+//MINIMUM NUMBER IN AN ARRAY
+int findMinimumInArray(int arr[], int size) {
+  //ans store variable
+  int minAns = INT_MAX;
+
+  for(int i=0; i<size; i++) {
+    if(arr[i] < minAns) {
+      minAns = arr[i];
+    }
+  }
+
+  return minAns;
+}
+
+
+//REVERSE AN ARRAY
+

04f4ba22dd4b46d5f942bd68bd87b5fd6b78a6b2
Sheetal Agrahari <145831862+sheetal-ag@users.noreply.github.com>
2023-11-07 14:08:14 +0545

    Update arr2.cpp

--- Week 3/arr2.cpp
+++ Week 3/arr2.cpp
@@ -11,6 +11,63 @@ int getUnique(int arr[], int n) {
   return ans;
 }
 
+
+//SORT 0s AND 1s:
+//using for and while loop
+
+void sortZeroOne(int arr[], int n) {
+  int zeroCount = 0;
+  int oneCount = 0;
+  //stepA: Count 0 and 1
+  for(int i=0; i<n; i++) {
+    if(arr[i] == 0) 
+      zeroCount++;
+    if(arr[i] ==1) 
+      oneCount++; 
+  }
+
+  //Step B: place all zeroes first
+  // int i;
+  // for(i =0; i<zeroCount; i++) {
+  //  arr[i] = 0;
+  // }
+  // //Step B: place all ones first
+  // for(int j = i; j<n ;j++) {
+  //  arr[j] = 1;
+  // }
+
+  //EASY WAY
+  int index = 0;
+
+  while(zeroCount--) {
+    arr[index] = 0;
+    index++;
+  }
+
+  while(oneCount--) {
+    arr[index] = 1;
+    index++;
+  }
+  
+}
+
+
+//SHIFT ARRAY TO RIGHT BY 1:
+//right ki disha me shift
+void shiftArray(int arr[], int n) {
+  //Step 1
+  int temp = arr[n-1];
+
+  //step2
+  //shift -> arr[i] = arr[i-1]
+  for(int i= n-1; i>=1; i--) {
+    arr[i] = arr[i-1];
+  }
+
+  //step3 copy temp into 0th index
+  arr[0] = temp;
+}
+
 int main(){
    int arr[] = {2,10,11,10,2,13,15,13,15};
    int n = 9;
